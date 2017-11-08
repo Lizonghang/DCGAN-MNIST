ca482494dccbb1ce5ad5ea553c775cc3a4eef5ab
lizonghang <870644199@qq.com>
2017-11-08 16:32:17 +0800

    tunning

--- extract_images.py
+++ extract_images.py
@@ -1,12 +1,15 @@
 from ops import *
 import input_data
-
+import os
 
 mnist = input_data.read_data_sets('dataset', one_hot=True)
 
 num = mnist.train.num_examples
 images = mnist.train.images
 labels = mnist.train.labels
+
+if not os.path.exists('dataset/train_images'):
+        os.makedirs('dataset/train_images')
 
 for i in range(num):
     data = images[i].reshape([1, 28, 28, 1])
